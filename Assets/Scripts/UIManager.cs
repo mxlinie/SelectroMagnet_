e68aaf39b41bb845589e235dc3117e8b8efb85fb
BPowell97 <bennypowell127@gmail.com>
2019-05-07 16:45:04 +1000

    Watch pick up & script + message appearance

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -8,10 +8,21 @@ public class UIManager : MonoBehaviour
 {
     public static UIManager Instance;
     public TextMeshProUGUI healthText;
+    public TextMeshProUGUI UIObjectText;
+
+    private void Start()
+    {
+        UIObjectText.text = ""; //Text is blank at the start of the game
+    }
 
     private void Awake()
     {
         Instance = this;
+    }
+
+    public void ToggleText(string itemName) //Toggle text is individually set in inspector for each item
+    {
+        UIObjectText.text = itemName;
     }
 
     public void UpdateHealth()
---
+++ Assets/Scripts/Watch.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Watch : MonoBehaviour
+{
+    public string message;
+
+    void OnTriggerStay(Collider other)
+    {
+        UIManager.Instance.ToggleText(message); //Message set in the inspector
+        if (other.gameObject.tag == "Player" && Input.GetKeyDown(KeyCode.E)) //if player is near watch object can be picked up by player 
+        {
+            Destroy(gameObject);
+            Debug.Log(name);
+            PickUp();
+        }
+
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        //lockText.gameObject.SetActive(true);
+        if (other.gameObject.tag == "Player")
+        {
+            UIManager.Instance.ToggleText(""); //message is blank when player is not in trigger zone
+        }
+
+    }
+
+    void PickUp()
+    {
+        UIManager.Instance.ToggleText(""); //message is blank when player picks up object
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
