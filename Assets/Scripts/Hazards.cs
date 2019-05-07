6b9a7207a7f08e0da90dbf3d36d80299704f6308
John Buizon <donjuanbuizon@gmail.com>
2019-05-07 23:02:59 +1000

    Falling Hazards (test)
    Added a couple of hazards with one having a script to where it will fall when player makes contact with collider

---
+++ Assets/Scripts/Hazards.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hazards : MonoBehaviour
+{
+    /*
+    public Transform player; //This creates a slot in the inspector where you can add your player
+    public float maxDistance = 5f; //This can be changed in the inspector to your liking
+    private Rigidbody rigidbody;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        rigidbody.useGravity = false;
+    }
+
+    void Update()
+    {
+        if (Vector3.Distance(player.position, transform.position) < maxDistance)
+        { //transform is the object that this script is attached to
+            rigidbody.useGravity = true;
+        }
+    }
+    */
+
+    public Rigidbody FallingObject;
+    public void OnTriggerEnter(Collider other)
+    {
+        if (FallingObject.isKinematic)
+            FallingObject.isKinematic = false;
+    }
+
+}
