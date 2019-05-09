7864c2af8453db7bda49cf0f95a0af3e22c248f6
BPowell97 <bennypowell127@gmail.com>
2019-05-09 17:22:12 +1000

    Pause esc key

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -10,9 +10,45 @@ public class UIManager : MonoBehaviour
     public TextMeshProUGUI healthText;
     public TextMeshProUGUI UIObjectText;
 
+    public GameObject pausePanel;
+    bool Paused = false;
+
     private void Start()
     {
         UIObjectText.text = ""; //Text is blank at the start of the game
+        pausePanel.gameObject.SetActive(false);
+    }
+
+    private void FixedUpdate()
+    {
+
+        {
+            if (Input.GetKey("escape"))
+            {
+                if (Paused == true)
+                {
+                    Time.timeScale = 1.0f;
+                    pausePanel.gameObject.SetActive(false);
+
+                    Paused = false;
+                }
+                else
+                {
+                    Time.timeScale = 0.0f;
+                    pausePanel.gameObject.SetActive(true);
+
+                    Paused = true;
+                }
+            }
+        }
+
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1.0f;
+        pausePanel.gameObject.SetActive(false);
+
     }
 
     private void Awake()
