97a1e9f138fee8f92222fd75d80edb9a5e7ef187
BenjaminARHall <43558480+BenjaminARHall@users.noreply.github.com>
2019-06-07 15:54:22 +1000

    --

--- Assets/Scripts/MoveGun.cs
+++ Assets/Scripts/MoveGun.cs
@@ -13,12 +13,14 @@ public class MoveGun : MonoBehaviour
 
     public float speed = 100f;
 
+    public int fR;
+
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody>(); // stating rigibody
     }
 
-    void FixedUpdate()
+    void FixedUpdate() //gun rotation
     {
         Vector3 mousePos = Input.mousePosition;
         mousePos.z = 5.23f;
@@ -31,39 +33,12 @@ public class MoveGun : MonoBehaviour
        float angleX = Mathf.Clamp(mousePos.x, -180, 0);
        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angleY));
 
+        //GunRotation();
+
         float horizontal = Input.GetAxis("Horizontal"); // horizontal movement no stated so HandleMovement will function
         HandleMovement(horizontal); //Calling function
-        Vector3 movement = new Vector3(acceleration, 0, 0);
-
-        if (Input.GetKey(KeyCode.A))
-        {
-
-            myRigidbody.AddForce(-movement * speed);
-            if (speed >= maxSpeed)
-            {
-                MaxSpeed();
-            }
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-
-            myRigidbody.AddForce(movement * speed);
-            if (speed >= maxSpeed)
-            {
-                MaxSpeed();
-            }
-        }
-
+        //Vector3 movement = new Vector3(acceleration, 0, 0);
     }
-
-    IEnumerator MaxSpeed()
-    {
-        yield return new WaitForSeconds(0);
-        maxSpeed = 10f;
-
-    }
-
 
     private void HandleMovement(float horizontal) //All horizontal movement through vector 2
     {
@@ -79,7 +54,7 @@ public class MoveGun : MonoBehaviour
 
     }
 
-    private void Flip(bool facingRight)
+    public void Flip(bool facingRight)
     {
         //facingRight = !facingRight;
         //transform.eulerAngles = new Vector3(0, -180, 0);
@@ -87,14 +62,30 @@ public class MoveGun : MonoBehaviour
         if (facingRight)
         {
             facingRight = false;
+            //fR = -1;
             transform.eulerAngles = new Vector3(0, -180, 0);
 
         }
         else
         {
             facingRight = true;
+            //fR = 0;
             transform.eulerAngles = new Vector3(0, 0, 0);
         }
     }
+
+    /*public void GunRotation()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = 5.23f;
+
+        Vector3 objectPos = Camera.main.WorldToScreenPoint(transform.position);
+        mousePos.x = mousePos.x - objectPos.x;
+        mousePos.y = mousePos.y - objectPos.y;
+
+        float angleY = Mathf.Clamp(mousePos.y, -60, 60);
+        float angleX = Mathf.Clamp(mousePos.x, -180, 0);
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angleY));
+    }*/
 
 }
