f95ac9812d6b821e74938f0d59f1f47c5e6cbd21
BPowell97 <bennypowell127@gmail.com>
2019-06-13 11:16:22 +1000

    Gun Movement Fixed

--- Assets/Scripts/MoveGun.cs
+++ Assets/Scripts/MoveGun.cs
@@ -10,10 +10,15 @@ public class MoveGun : MonoBehaviour
 
     private float maxSpeed;
     private float acceleration = 2f;
+    public GameObject Sgun; //SelectroMagnet Gun
+    public bool FacingRight = true;
+
+    public float Horizontal;
+    public float MS = 7.0f;
 
     public float speed = 100f;
 
-    public int fR;
+
 
     void Start()
     {
@@ -22,7 +27,7 @@ public class MoveGun : MonoBehaviour
 
     void FixedUpdate() //gun rotation
     {
-        Vector3 mousePos = Input.mousePosition;
+        /*Vector3 mousePos = Input.mousePosition;
         mousePos.z = 5.23f;
 
        Vector3 objectPos = Camera.main.WorldToScreenPoint(transform.position);
@@ -37,10 +42,32 @@ public class MoveGun : MonoBehaviour
 
         float horizontal = Input.GetAxis("Horizontal"); // horizontal movement no stated so HandleMovement will function
         HandleMovement(horizontal); //Calling function
-        //Vector3 movement = new Vector3(acceleration, 0, 0);
+        //Vector3 movement = new Vector3(acceleration, 0, 0);*/
+        // I put the aiming biz into a separate event to keep the Update/Fixed Update section clean.
+        AimGun();
+
+        // Binding the camera to the character
+        //Cam.gameObject.transform.position = this.gameObject.transform.position + CamOffset;
+
+
+        // This is what I did for simple left-right movement. 
+        Horizontal = Input.GetAxis("Horizontal") * MS;
+        myRigidbody.velocity = new Vector3(Horizontal, 0, 0);
+        // The "&& FacingRight" means it will only change the bool when we are changing direction
+        // It won't try to do anything if we press right twice
+        if (Input.GetAxis("Horizontal") > 0f && !FacingRight)
+        {
+            FacingRight = true;
+            this.transform.Rotate(0, -180f, 0);
+        }
+        if (Input.GetAxis("Horizontal") < 0f && FacingRight)
+        {
+            FacingRight = false;
+            this.transform.Rotate(0, 180f, 0);
+        }
     }
 
-    private void HandleMovement(float horizontal) //All horizontal movement through vector 2
+    /*private void HandleMovement(float horizontal) //All horizontal movement through vector 2
     {
         myRigidbody.velocity = new Vector2(horizontal * maxSpeed, myRigidbody.velocity.y);
         if (horizontal < -0.1f)
@@ -52,9 +79,37 @@ public class MoveGun : MonoBehaviour
             Flip(false); //Player remains in same position
         }
 
+    }*/
+
+    void AimGun()
+    {
+        // Pretty sure the code here isn't any different to yours.
+        Vector3 mousePos = Input.mousePosition;
+
+        // Because my PlayerController binds the camera, I use my CamOffset.z to allow for variable distances.
+        mousePos.z = 5.23f;
+
+        Vector3 gunPos = Camera.main.WorldToScreenPoint(Sgun.transform.position);
+        mousePos.x = mousePos.x - gunPos.x;
+        mousePos.y = mousePos.y - gunPos.y;
+        float angleY = Mathf.Clamp(mousePos.y, -60, 60);
+        float angleX = Mathf.Clamp(mousePos.x, -180, 0);
+
+        // --- THE ACTUAL FIX ---
+        // This is the "Magic" that fixes your several layers of flipping hell
+        // The code is pretty much identical, just set the 180 degree rotation pending a FacingRight bool check.
+        if (FacingRight)
+        {
+            Sgun.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angleY));
+        }
+        else if (!FacingRight)
+        {
+            Sgun.transform.rotation = Quaternion.Euler(new Vector3(0, 180, angleY));
+        }
+
     }
 
-    public void Flip(bool facingRight)
+    /*public void Flip(bool facingRight)
     {
         //facingRight = !facingRight;
         //transform.eulerAngles = new Vector3(0, -180, 0);
@@ -72,8 +127,8 @@ public class MoveGun : MonoBehaviour
             //fR = 0;
             transform.eulerAngles = new Vector3(0, 0, 0);
         }
-    }
-
+    }*/
+    
     /*public void GunRotation()
     {
         Vector3 mousePos = Input.mousePosition;
