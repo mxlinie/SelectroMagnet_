1104a4b099329f928d0628123bc8a92eb06e4f77
BPowell97 <bennypowell127@gmail.com>
2019-06-23 18:06:27 +1000

    Add Gun Laser

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    public float laserBeamLength;
+    private LineRenderer lineRenderer;
+
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 endPosition = transform.position + (transform.right * laserBeamLength);
+        lineRenderer.SetPositions(new Vector3[] { transform.position, endPosition });
+    }
+
+}
