663ea2a7b981400211131f460e95b2075d1f4d7e
Faeblaze <mjbstankovic27@gmail.com>
2019-06-28 10:16:53 +1000

    Minus Health Particle

--- Assets/Scripts/Trap.cs
+++ Assets/Scripts/Trap.cs
@@ -7,6 +7,8 @@ public class Trap : MonoBehaviour
     public int hitPlayerDamage = 1;
     public GameObject trapTrigger;
 
+    public GameObject trapHitEffect; //Particle Effect for hitting a trap
+
     [SerializeField] private AudioClip hitPlayer;
 
     private void OnTriggerEnter(Collider hitInfo)
@@ -15,6 +17,7 @@ public class Trap : MonoBehaviour
         if (player != null)
         {
             AudioManager.Instance.phSFX(hitPlayer); //when player enters the trigger zone sound clip plays
+            Instantiate(trapHitEffect, transform.position, Quaternion.identity); //When player hits trap particle system with play
             player.PlayerTakeDamage(hitPlayerDamage);
         }
 
