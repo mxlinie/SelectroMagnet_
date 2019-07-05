5177b540ce0527b729b9ff5e5910581e4d8607c8
Ben Powell <43558471+BPowell97@users.noreply.github.com>
2019-07-05 10:24:38 +1000

    Fixed moving platform level 2
    Made it so that platforms can move up and down and left to right on same script.

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -4,28 +4,59 @@ using UnityEngine;
 
 public class Platform : MonoBehaviour
 {
+    //public Transform[] Waypoints;
+
+    //public float platformSpeed = 2;
+    //Vector3 platformPosition;
+    //    public int CurrentPoint = 0;
+    ////public float posY;
+    ////public float posZ;
+    ////https://answers.unity.com/questions/827225/moving-platform-help-c.html
+
+    //private void Start()
+    //{
+
+    //}
+
+    //void FixedUpdate()
+    //{
+    //   // new Vector3 platformPosition(0,transform.position.y, transform.position.z); //error when I opened game
+    //    if(transform.position.z != Waypoints[CurrentPoint].transform.position.z)
+    //    {
+    //        transform.position = Vector3.MoveTowards(transform.position, Waypoints[CurrentPoint].transform.position, platformSpeed * Time.deltaTime);
+    //    }
+
+    //    if(transform.position.z == Waypoints[CurrentPoint].transform.position.z)
+    //    {
+    //        CurrentPoint += 1;
+    //    }
+    //    if(CurrentPoint >= Waypoints.Length)
+    //    {
+    //        CurrentPoint = 0;
+    //    }
+
+
+    //}
     public Transform[] Waypoints;
+    public float moveSpeed = 3;
+    //public float rotateSpeed = 0.5f;
+    //public float scaleSpeed = 0f;
+    public int CurrentPoint = 0;
 
-    public float platformSpeed = 2;
-
-        public int CurrentPoint = 0;
-    //public float posY;
-    //public float posZ;
-    //https://answers.unity.com/questions/827225/moving-platform-help-c.html
-    
     void FixedUpdate()
     {
-        //new Vector3 platPos(0,posY,posZ); //error when I opened game
-        if(transform.position.z != Waypoints[CurrentPoint].transform.position.z)
+        if (transform.position != Waypoints[CurrentPoint].transform.position)
         {
-            transform.position = Vector3.MoveTowards(transform.position, Waypoints[CurrentPoint].transform.position, platformSpeed * Time.deltaTime);
+            transform.position = Vector3.MoveTowards(transform.position, Waypoints[CurrentPoint].transform.position, moveSpeed * Time.deltaTime);
+           // transform.rotation = Quaternion.Lerp(transform.rotation, Waypoints[CurrentPoint].transform.rotation, rotateSpeed * Time.deltaTime);
+            //transform.localScale = Vector3.Lerp(transform.localScale, Waypoints[CurrentPoint].transform.localScale, scaleSpeed * Time.deltaTime);
         }
 
-        if(transform.position.z == Waypoints[CurrentPoint].transform.position.z)
+        if (transform.position == Waypoints[CurrentPoint].transform.position)
         {
             CurrentPoint += 1;
         }
-        if(CurrentPoint >= Waypoints.Length)
+        if (CurrentPoint >= Waypoints.Length)
         {
             CurrentPoint = 0;
         }
