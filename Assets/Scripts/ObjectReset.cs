cc4623246a224995705aa834f72065827340daa3
BPowell97 <43558471+BPowell97@users.noreply.github.com>
2019-07-05 16:20:16 +1000

    Press "R" Key to reset puzzles

--- Assets/Scripts/ObjectReset.cs
+++ Assets/Scripts/ObjectReset.cs
@@ -6,13 +6,19 @@ public class ObjectReset : MonoBehaviour
 {
     public List<GameObject> platforms = new List<GameObject>();
 
-    private Vector3 orgPosPlatforms;
+    public List<Vector3> orgPos = new List<Vector3>();
+
+    //pubic gamobject attached to gun.cs
+    public GameObject selGun;
 
     // Start is called before the first frame update
     void Start()
     {
-        orgPosPlatforms = transform.position;
-        
+        //orgPosPlatforms = transform.position;
+        foreach (GameObject polPlat in platforms)
+        {
+            orgPos.Add(polPlat.transform.localPosition);
+        }
     }
 
     // Update is called once per frame
@@ -20,7 +26,26 @@ public class ObjectReset : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.R))
         {
-            //platforms = orgPosPlatforms;
+            ResetPol();
+            Debug.Log("key pressed");
+        }
+    }
+
+    public void ResetPol()
+    {
+        int i = 0;
+        //Set null reference
+        selGun.GetComponent<Gun>().positivePolarityObject = null;
+        selGun.GetComponent<Gun>().negativePolarityObject = null;
+        
+        foreach (GameObject polPlat in platforms)
+        {
+            Debug.Log("Index "+i+" Current pos = "+polPlat.transform.localPosition+" -- Target pos = " + orgPos[i]);
+            polPlat.transform.localPosition = orgPos[i];
+            polPlat.GetComponent<Renderer>().material.color = new Color32(255, 255, 255, 255);
+            polPlat.GetComponent<Polarity>().thisPole = Pole.Neutral;
+            i++;
         }
     }
 }
+
