92b99219728a2ed2f563820cbe7eafba4f8e60cd
BPowell97 <bennypowell127@gmail.com>
2019-07-11 14:47:04 +1000

    Player Guide in start menu

--- Assets/Scripts/Respawn.cs
+++ Assets/Scripts/Respawn.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class Respawn : MonoBehaviour
 {
+    public static Respawn Instance;
+
     [SerializeField] private Transform player;
     [SerializeField] private Transform respawnPoint;
 
     private void OnTriggerEnter(Collider other)
+    {
+        PlayerRespawn(); //Moved function so it would appear on script
+    }
+
+    public void PlayerRespawn()
     {
         player.transform.position = respawnPoint.transform.position;
     }
