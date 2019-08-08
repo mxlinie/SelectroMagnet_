ee7fb06e36aeacc4ca345f43f88b86a7ce7671a4
BPowell97 <bennypowell127@gmail.com>
2019-08-08 21:14:57 +1000

    All Menu's are changed to new images + buttons + start/end cutscene (with scrolling text)

---
+++ Assets/Scripts/Dialogue.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Dialogue : MonoBehaviour
+{
+    //Variables
+    public TextMeshProUGUI UIText;
+    public string[] sentences; //Sentences you can type in inspector
+    private int index;
+    public float textSpeed; //How fast the dialogue goes
+    public GameObject nextSentenceButton;
+
+    //public Animator UITextAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Type());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(UIText.text == sentences[index])
+        {
+            nextSentenceButton.SetActive(true);
+        }
+    }
+
+    IEnumerator Type()
+    {
+        foreach(char letter in sentences[index].ToCharArray())
+        {
+            UIText.text += letter;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+    public void StartNextSentence()
+    {
+        //UITextAnim.SetTrigger("Change");
+        nextSentenceButton.SetActive(false);
+
+        if(index < sentences.Length - 1)
+        {
+            index++;
+            UIText.text = "";
+            StartCoroutine(Type());
+        }
+        else
+        {
+            UIText.text = "";
+        }
+    }
+}
